f84f390e4981f33a3f0482d867a33eb508d48d9f
bbtrombone <bbtrombone@sbcglobal.net>
2021-06-28 13:53:31 -0700

    added post_value.js

---
+++ src/pots_value.js
@@ -0,0 +1,13 @@
+let potsHashTable = {
+    "uncommon": [  
+        [ [2,"lesser arcane"], [2, "lesser affinity"] ],
+        [ [2,"lesser arcane"], [2, "lesser affinity"] ],
+        [ [3,"lesser arcane"], [2, "lesser affinity"] ],
+        [ [3,"lesser arcane"], [3, "lesser affinity"] ],
+        [ [5,"lesser arcane"], [3, "lesser affinity"] ],
+        [ [6,"lesser arcnae"], [3, "lesser affinity"] ]   
+    ],
+    "rare": [          ],
+    "epic": [          ],
+    "lego": [          ],
+}
