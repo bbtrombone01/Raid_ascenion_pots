666a458b9114b5ac495aa38c167bc2ea21c9eda9
bbtrombone <bbtrombone@sbcglobal.net>
2021-06-28 15:47:34 -0700

    added potionCountReducer to store

--- src/Store/reducers/index.js
+++ src/Store/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
 import  homePageReducer  from "./homePageReducer"
+import potionCounteReducer from "./potionCountReducer";
 
-
-const rootReducer = combineReducers({homePageReducer})
+const rootReducer = combineReducers({homePageReducer, potionCounteReducer})
 
 
 export default rootReducer
