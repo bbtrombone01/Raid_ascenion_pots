1a695cb0e941778836d04ca58a1174247855d3ee
bbtrombone <bbtrombone@sbcglobal.net>
2022-01-24 15:12:03 -0800

    updated form.css with media rules for mobile users

--- src/componets/ParagraphBlurb.js
+++ src/componets/ParagraphBlurb.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 function ParagrahBlurb (){
 
-        return <p>
-            Hello this is a tool for the game “Raid Shadow Legends”, focused on the ascension mechanic. I have found it hard to know what resources are needed to gain ascension levels, due to a lack of transparency and shifting needs as you acquire new champions. In order to address this, I created a tool to allow players to enter information about their ascension needs. As you fill out the form bellow, this tool provides a count of all ascension material needed.
-        </p>   
+        return <div>
+
+            <p>Hello this is a tool for the game “Raid Shadow Legends”, focused on the ascension mechanic. I have found it hard to know what resources are needed to gain ascension levels, due to a lack of transparency and shifting needs as you acquire new champions. In order to address this, I created a tool to allow players to enter information about their ascension needs. As you fill out the form bellow, this tool provides a count of all ascension material needed.</p> 
+            <p className="visableOnlyMobile"> This app was designed for a web broswer, for the best exprince please use a desktop</p>  
+
+        </div>
 }
 
 export default ParagrahBlurb
